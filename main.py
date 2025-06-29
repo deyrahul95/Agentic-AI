b51efb3292df46bb2a31fc6b92538b11f941dbbd
Rahul Dey <rahuldey9623@gmail.com>
2025-06-29 05:30:11 +0000

    Add basic chat app using langchain and Ollama model

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
+from langchain.chat_models import init_chat_model
+from langchain_core.language_models import BaseChatModel
+
+from dotenv import load_dotenv
+
+import os
+
+load_dotenv()
+
+
+def get_ollama_model() -> BaseChatModel:
+    """
+    Create an instance of local OLLAMA model
+
+    Returns:
+        an instance of 'BaseChatModel'
+    """
+    base_url: str = str(os.getenv("OLLAMA_URL"))
+
+    model = init_chat_model("llama3.2:1b", model_provider="ollama", base_url=base_url)
+    return model
+
+
 def main():
-    print("Hello from agenticai!")
+    model = get_ollama_model()
+
+    while True:
+        query: str = input("Please enter your query (or '/bye' to end this chat) => ")
+
+        if query.lower() == "/bye":
+            return
+
+        response = model.invoke([{"role": "user", "content": query}])
+        print(response.text())
 
 
 if __name__ == "__main__":
